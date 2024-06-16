cd00b8fe53b385f8cc7933bd8581b3f2039a8883
Lyhua-tech <slyhour69@gmail.com>
2024-06-16 21:28:50 +0700

    add function to create note

---
+++ src/js/app.js
@@ -0,0 +1,42 @@
+function createNote(event) {
+    event.preventDefault();
+    let textInput = document.getElementById('text-input').value;
+    let card = document.createElement('my-card');
+    card.setAttribute('text', textInput);
+    document.getElementById('card-container').appendChild(card);
+}
+
+function deleteNote(event) {
+    const card = event.target.closest('my-card');
+    if (card) {
+        document.getElementById('card-container').removeChild(card);
+    }
+}
+function editNote() {
+
+}
+
+class CreateNote extends HTMLElement {
+    constructor() {
+        super();
+    }
+    
+    connectedCallback() {
+        let text = this.getAttribute('text');
+        this.innerHTML = `
+            <div class='note-container'>
+                <div class="title-box">
+                    <h1>${text}</h1>
+                    <button class="remove-note">x</button>
+                </div>
+                <p>
+
+                </p>
+            </div>
+        `;
+
+        this.querySelector('.remove-note').addEventListener('click', deleteNote);
+    }
+}
+
+customElements.define('my-card', CreateNote);
